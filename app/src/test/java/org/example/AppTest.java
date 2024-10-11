64c38eb5bb93512f66d776ceb7bacd3b5a0d733b
gitubanana <ptyolo666@gmail.com>
2024-10-11 20:51:17 +0900

    fix(AppTest): remove useless methods

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -4,10 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
 class AppTest extends IOTest {
     @Test
     @DisplayName("1이 나와야 하는 테스트")
