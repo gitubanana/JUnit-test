260edeba418fc80a4f72e2cddb02ea6c30cbe6ab
gitubanana <ptyolo666@gmail.com>
2024-10-14 22:00:53 +0900

    style(AppTest): remove unused import

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest extends IOTest {
     @Test
